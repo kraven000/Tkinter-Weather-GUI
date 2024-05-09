942177aac2b5ffb9e47324b194d65625447cacad
kraven <akhisanjayverma26@gmail.com>
2024-05-10 00:14:49 +0530

    adding some png files

--- main.py
+++ main.py
@@ -1,127 +1,216 @@
-from tkinter import Tk, Button, Entry, StringVar, PhotoImage, Label
-import requests as req
-from random import shuffle, randint
-from os import getcwd, getenv, listdir
+from tkinter import Tk,Label,Entry,Button,Frame,PhotoImage,StringVar
+from time import strftime
+from requests import get
+from os import getenv
 from dotenv import load_dotenv
+from memory_profiler import memory_usage
+from time import perf_counter
 
 load_dotenv()
+time = strftime("%I:%M %p")
 
-
-def weather():
-    if len(weth.get()) != 0:
+def weather(events=None):
+    if len(store_area.get()) != 0:
         try:
-            location = str(weth.get())
+            # Adding time and 'Current Weather' text
+            time_text.config(text=f"{time} ")
+            current_weather_text.config(text="Current Weather")
+            
+            # editing location to make it fit for url
+            location = str(store_area.get()).strip().lower()
             location = location.replace(" ", "%20")
-            url_response = req.get(
-                url=f"https://api.weatherapi.com/v1/current.json?key={getenv('api_key')}&q={location}&aqi=no"
-            ).json()
-            # location_weather = load(url)
+            
+            # json file of weather
+            url_response = get(url=f"https://api.weatherapi.com/v1/current.json?key={getenv('api_key')}&q={location}&aqi=no").json()
             del location
-            weather_res = f"""Temperature:- {url_response["current"]["temp_c"]}\u00b0C/{url_response["current"]["temp_f"]}\u00b0F\nRegion:- {url_response["location"]["region"]}\nCity:- {url_response["location"]["name"]}\nCondition:- {url_response["current"]["condition"]["text"]}"""
+            
+            # degree symbol
+            degree_symbol = u"\u00b0"
+            
+            # changing location and region
+            location_city_region.config(text=f"""{url_response["location"]["name"]}, {url_response["location"]["region"]}, {url_response["location"]["country"]}""")
+            
+            # adding weather in degree celsius
+            weather_show.config(text=f"""{url_response["current"]["temp_c"]}{degree_symbol}C /""",font="Helvetica 60 bold",bg="#101010",fg="#FFFFFF")
+            
+            # adding condition            
+            weather_condition.config(text=f"""{url_response["current"]["condition"]["text"]}|Feels Like:- {url_response["current"]["feelslike_c"]}{degree_symbol}C""",font="Consolas 12 bold",bg="#101010",fg="#FFFFFF")
+            
+            if len(str(url_response["current"]["temp_c"]))==3:
+                celsius_fahrenhiet_button.place(x=680,y=110)
+            else:
+                celsius_fahrenhiet_button.place(x=705,y=110)
+            
+            
+            # adding the details of bluebox
+            windspeed.config(text=f"""{url_response["current"]["wind_kph"]} km/hr""",bg="#0066CC",fg="#202020",font="default 16 bold")
+            pressure.config(text=f"""{url_response["current"]["pressure_mb"]} mb""",bg="#0066CC",fg="#202020",font="default 16 bold")
+            humidity.config(text=f"""{url_response["current"]["humidity"]}%""",bg="#0066CC",fg="#202020",font="default 16 bold")
+            
 
-            showing_weather.config(text=weather_res)
-            showing_weather.after(500, weather)
-        except:
-            showing_weather.config(
-                text="This Location doesn't Exist Please\ncheck the spelling."
-            )
+            weather_condition_for_bluerect = str(url_response["current"]["condition"]["text"]).split()
+            
+            if len(weather_condition_for_bluerect)>1:
+                weather_condition_for_bluerect = weather_condition_for_bluerect[0]+" "+weather_condition_for_bluerect[1]
+            else:
+                weather_condition_for_bluerect = weather_condition_for_bluerect[0]
+            
+            condition.config(text=f"""{weather_condition_for_bluerect}""",bg="#0066CC",fg="#202020",font="default 16 bold")
+            
+            del weather_condition_for_bluerect
+            return url_response
+        
+        except Exception as e:
+            print(e)
+            weather_show.config(text="This Location doesn't Exist",font="Helvetica 14 bold")
     else:
-        location = req.get(("https://ipinfo.io/json")).json()
+        # taking location through ipinfo
+        location = get(("https://ipinfo.io/json")).json()
         location = location["city"]
-        url = req.get(
-            (
-                f"https://api.weatherapi.com/v1/current.json?key={getenv('api_key')}&q={location}&aqi=no"
-            )
-        ).json()
+        
+        # weather
+        url = get(f"https://api.weatherapi.com/v1/current.json?key={getenv('api_key')}&q={location}&aqi=no").json()
         del location
         return url
 
 
+def celsius():
+    '''The function to show weather in Degree Celsius if button is pressed'''
+    
+    response = weather()
+    
+    degree_symbol = u"\u00b0"
+    
+    celsius_fahrenhiet_button.config(text=f"{degree_symbol}F")
+    
+    if len(str(response["current"]["temp_c"]))==3:
+        celsius_fahrenhiet_button.place(x=680,y=110)
+    else:
+        celsius_fahrenhiet_button.place(x=705,y=110)
+    
+    celsius_fahrenhiet_button.config(command=fahrenhiet,bg="#101010",fg="#FFFFFF")
+    
+    weather_show.config(text=f"""{response["current"]["temp_c"]}{degree_symbol}C /""",font="Helvetica 60 bold",bg="#101010",fg="#FFFFFF")
+    weather_condition.config(text=f"""{response["current"]["condition"]["text"]}|Feels Like:- {response["current"]["feelslike_c"]}{degree_symbol}C""",font="Consolas 14 bold",bg="#101010",fg="#FFFFFF")
+
+
+def fahrenhiet():
+    '''The function to show weather in Fahrenhiet when button is pressed'''
+    
+    response = weather()
+    
+    degree_symbol = u"\u00b0"
+    
+    celsius_fahrenhiet_button.config(text=f"{degree_symbol}C")
+    
+    if len(str(response["current"]["temp_f"]))==5:
+        celsius_fahrenhiet_button.place(x=750,y=110)
+    else:
+        celsius_fahrenhiet_button.place(x=705,y=110)
+    
+    celsius_fahrenhiet_button.config(command=celsius,bg='#101010',fg="#FFFFFF")
+    
+    weather_show.config(text=f"""{response["current"]["temp_f"]}{degree_symbol}F /""",font="Helvetica 60 bold",bg="#101010",fg="#FFFFFF")
+    weather_condition.config(text=f"""{response["current"]["condition"]["text"]}|Feels Like:- {response["current"]["feelslike_f"]}{degree_symbol}F""",font="Consolas 14 bold",bg="#101010",fg="#FFFFFF")
+
+
 def main():
+    
+    global time_text, current_weather_text, weather_show, store_area, weather_condition, detailed_desc, windspeed, condition, pressure, humidity, celsius_fahrenhiet_button, weather_show,location_city_region
+    
+    
+    # root.iconphoto(True,icon)
     root = Tk()
-    root.maxsize(627, 427)
-
-    root.title("Weather")
-    # setting icon
-    icon = PhotoImage(
-        file="/media/newdriveee/programs/githubb/Tkinter-Weather-GUI/png/icon.png"
-    )
-
-    root.iconphoto(True, icon)
-
-    # Making a background image
-    # # Making a list of background images
-
-    image_list = [
-        image
-        for image in listdir(
-            "/media/newdriveee/programs/githubb/Tkinter-Weather-GUI/png"
-        )
-    ]
-
-    image_list.remove("icon.png")
-    shuffle(image_list)
-
-    image0 = PhotoImage(
-        file=f"{getcwd()}/png/{image_list[randint(0,len(image_list)-1)]}"
-    )
-
-    def change_background_image():
-        new_image = PhotoImage(
-            file=f"{getcwd()}/png/{image_list[randint(0, len(image_list)-1)]}"
-        )
-
-        showing_image.config(image=new_image)
-        showing_image.image = new_image
-        showing_image.after(60000, change_background_image)
-
-    showing_image = Label(root, image=image0)
-    showing_image.pack()
-
-    change_background_image()
-
-    # Making a entry to enter a city
-    # # Entry
-    global weth
-    weth = StringVar()
-    Entry(root, textvariable=weth, width=40).place(x=150, y=0)
-
-    # # Making Search Button
-    Button(
-        root,
-        text="SUBMIT!!",
-        font="Aerial 7 bold",
-        background="#404040",
-        foreground="#FFFFFF",
-        command=weather,
-    ).place(x=430, y=0)
-
-    # Making Exit Button
-    Button(
-        root,
-        text="EXIT!!",
-        font="Aerial 7 bold",
-        background="#404040",
-        foreground="#FFFFFF",
-        command=exit,
-    ).place(x=540, y=0)
-
-    # Showing wheather
-    global showing_weather
-    showing_weather = Label(
-        root,
-        text=f"""Temperature:- {weather()["current"]["temp_c"]}\u00b0C/{weather()["current"]["temp_f"]}\u00b0F\nRegion:- {weather()["location"]["region"]}\nCity:- {weather()["location"]["name"]}\nCondition:- {weather()["current"]["condition"]["text"]}""",
-        borderwidth=0,
-        font="Aerial 12 bold",
-        border=0,
-        background="#606060",
-        foreground="#FFFFFF",
-    )
-
-    showing_weather.place(x=210, y=160)
-
+    root.title("Weather App")
+    root.geometry("1000x505")
+    root.resizable(False,False)
+    root.configure(bg="#101010")
+    
+    # Images
+    gui_icon = PhotoImage(file="icon.png")
+    weather_icon = PhotoImage(file="weather_icon.png")
+    search_icon = PhotoImage(file="search_icon.png")
+    location_icon = PhotoImage(file="location_icon.png")
+    
+    # Icon
+    root.iconphoto(False,gui_icon)
+    
+    #entry widget
+    store_area = StringVar()
+    entry_widget = Entry(root,textvariable=store_area,width=24,justify="center",background="#404040",foreground="#FFFFFF",border=0,font="Consolas 25 bold")
+    entry_widget.place(x=10,y=10)
+    entry_widget.bind("<Return>",weather)
+    entry_widget.focus()
+    
+    # i am declaring default response as global to avoid calling weather soo many time and only calling it one time
+    default_response = weather()
+    
+    
+    #button for search
+    Button(root,image=search_icon,bg="#101010",borderwidth=0,border=0,command=weather).place(x=500,y=6)
+    
+    # current weather text to show after user press search button or enter
+    current_weather_text = Label(root,text="",font="Helvetica 20 bold",bg="#101010",fg="#FFFFFF")
+    current_weather_text.place(x=6,y=60)
+    
+    # to show current time when press search button or enter
+    time_text = Label(root,text=f"",font="Helvetica 20 bold",bg="#101010",fg="#FFFFFF")
+    time_text.place(x=6,y=100)
+    
+    # to show city and region
+    Label(image=location_icon,bg="#101010").place(x=560,y=6)
+    
+    location_city_region = Label(root,text=f"""{default_response["location"]["name"]}, {default_response["location"]["region"]}, {default_response["location"]["country"]}""",font="Helvetica 16 bold",bg="#101010",fg="#FFFFFF")
+    location_city_region.place(x=605,y=12)
+    
+    # showing weather icon
+    Label(root,image=weather_icon,bg="#101010").place(x=160,y=100)
+    
+    # showing weather
+    degree_symbol = u"\u00b0"
+    weather_show = Label(root,text=f"""{default_response["current"]["temp_c"]}{degree_symbol}C /""",font="Helvetica 60 bold",fg="#FFFFFF",bg="#101010")
+    weather_show.place(x=412,y=115)
+    
+    # showing condition
+    weather_condition = Label(root,text=f"""{default_response["current"]["condition"]["text"]}|Feels Like:- {default_response["current"]["feelslike_c"]}{degree_symbol}C""",font="Consolas 12 bold",bg="#101010",fg="#FFFFFF")
+    weather_condition.place(x=422,y=200)
+    
+    # Button to switch between degree celsius and degree fahrenheit
+    celsius_fahrenhiet_button = Button(root,text=f"{degree_symbol}F",font="Helvetica 60 bold",fg="#FFFFFF",bg="#101010",border=0,borderwidth=0,command=fahrenhiet)
+    if len(str(default_response["current"]["temp_c"]))==3:
+        celsius_fahrenhiet_button.place(x=680,y=110)
+    else:
+        celsius_fahrenhiet_button.place(x=705,y=110)
+    
+    
+    # Frame for Background
+    frame = Frame(root,bg="#0066CC",width=1100,height=120)
+    frame.place(x=-10,y=390)
+    
+    # To show Wind Speed, HUmidity, Description and Pressure
+    Label(root,text="WIND SPEED         HUMIDITY          DESCRIPTION              PRESSURE",font="Default 18 bold",fg="#FFFFFF",bg="#0066CC",border=0,borderwidth=0).place(x=0,y=400)
+    
+    windspeed = Label(root,text=f"""{default_response["current"]["wind_kph"]} km/hr""",bg="#0066CC",fg="#202020",font="default 16 bold")
+    windspeed.place(x=5,y=450)
+    
+    humidity = Label(root,text=f"""{default_response["current"]["humidity"]}%""",bg="#0066CC",fg="#202020",font="default 16 bold")
+    humidity.place(x=280,y=450)
+    
+    condition = Label(root,text=f"""{default_response["current"]["condition"]["text"]}""",bg="#0066CC",fg="#202020",font="default 16 bold")
+    condition.place(x=455,y=450)
+    
+    pressure = Label(root,text=f"""{default_response["current"]["pressure_mb"]} mb""",bg="#0066CC",fg="#202020",font="default 16 bold")
+    pressure.place(x=760,y=450)
+    
+    
     root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    start = perf_counter()
+    a = memory_usage(main)
+    end = perf_counter()
+    print("Runtime:- ",(end-start)+1)
+    print("Memory Usage:- ",sum(a)/len(a))
+    # print(sum(memory_usage(main))
